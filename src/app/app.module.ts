4c0d1e1902290459d0f095e5ae5cd92de34be1d1
Laka Nodia <laka.nodia@gmail.com>
2022-11-13 11:26:44 +0400

    Create backend API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './data.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientInMemoryWebApiModule.forRoot(DataService)
+    // to avoid creating multiple instances of DataService
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
